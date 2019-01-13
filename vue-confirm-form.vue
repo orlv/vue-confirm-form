0e3aa4bc1687eedc14bf0002ce47dcd326507f05
Oleg Orlov <oleg.orlv@gmail.com>
2019-01-13 18:05:35 +0300

    Fix checkbox

--- vue-confirm-form.vue
+++ vue-confirm-form.vue
@@ -36,6 +36,11 @@
                                 <option v-for="option in formFields[fieldName]">{{ option }}</option>
                             </select>
                         </td>
+                        <td v-else-if="typeof fieldValue === 'boolean'">
+                            <input :id="`vue-confirm-form-${fieldName}`" v-model="form[fieldName]" title=""
+                                   class="css-checkbox" type="checkbox">
+                            <label :for="`vue-confirm-form-${fieldName}`" class="css-label"></label>
+                        </td>
                     </tr>
                     <tr>
                         <td colspan="2" class="center p-1 font-1_3">
